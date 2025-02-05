acef7fd664b623dffa1195b7dbb41bc44f9a9ed6
R3g3n7 <polidoroalessio9@gmail.com.com>
2025-02-05 19:28:17 +0100

    BUTTONS OK

--- app/views/components/hero.php
+++ app/views/components/hero.php
@@ -8,8 +8,9 @@
             <p class="text">
             Discover the ancient wisdom of the samurai way. Explore our collection of teachings and philosophies that have guided warriors for centuries.
             </p>
+            <a href="#contact"><button class="btn-primary">Contactez-nous</button></a>
+    <a href="#horaire"><button class="btn-secondary">Horaire</button></a>
         </div>
     </div>
-    <a href="#contact"><button class="btn-primary">Contactez-nous</button></a>
-    <a href="#horaire"><button class="btn-secondary">Horaire</button></a>
+    
 </section>
