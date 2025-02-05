3e48c8e4db785701dae1567d1a760730801f95a4
Nacho <kami.labs@proton.me>
2025-02-05 22:35:58 +0100

    update accueil structure

--- app/views/pages/accueil.php
+++ app/views/pages/accueil.php
@@ -2,40 +2,9 @@
 <section class="home-container" >
     
     <?php include __DIR__ . '/../components/hero.php'; ?>
-    
-    <section id="#horaire">
-        <h2>Horaires</h2>
-        <div class="cards" >
-            <ul>
-                <li>Mardi
-                    <ul><li>
-                        19h30/21h00
-                    </li></ul>
-                </li>
-            </ul>
-            <ul>
-                <li>Mercredi
-                    <ul><li>
-                        16h00/17h30
-                    </li></ul>
-                </li>
-            </ul>
-        </div>
-        <div class="cards">
-            <ul>
-                <li>Cours enfants
-                    <ul>Vendredi</ul>
-                    <ul><li>
-                        18h00/19h15
-                    </li></ul>
-                </li>
-                <li>Cours adultes
-                    <ul><li>
-                        19h15/20h45
-                    </li></ul>
-                </li>
-            </ul>
-        </div>
-    <img src="/public\assets\img\horaires_img.png" alt="Image d'un dojo">
-    </section>
+    <?php include __DIR__ . '/../components/horaire.php'; ?>
+    <?php include __DIR__ . '/../components/licence.php'; ?>
+    <?php include __DIR__ . '/../components/shop.php'; ?>
+    <?php include __DIR__ . '/../components/gallery.php'; ?>
+
 </section>
