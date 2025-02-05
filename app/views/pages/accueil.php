4ec165c47b8184cacb18a581c1e772d7f5252880
R3g3n7 <polidoroalessio9@gmail.com.com>
2025-02-05 22:51:53 +0100

    font //ok

--- app/views/pages/accueil.php
+++ app/views/pages/accueil.php
@@ -2,18 +2,30 @@
 <section class="home-container" >
     
     <?php include __DIR__ . '/../components/hero.php'; ?>
+
+    <h2>Horaires</h2>
     
-    <section class="horaire" id="#horaire">
+    <section class="horaire" id="horaire">
 
-        <h2>Horaires</h2>
+        
 
-        <div class="cards" >
+        <div class="cards">
 
          <h4> COURSE ADULTES </h4>
          <p> MARDI</p> 
          <p> 19H30 - 21H00</p> 
          <p> VENDREDI </p>
          <p> 19H15 - 20H45 </p> 
+
+        </div>
+
+        <div class="cards">
+
+        <h4> COURSE ADULTES </h4>
+        <p> MARDI</p> 
+        <p> 19H30 - 21H00</p> 
+        <p> VENDREDI </p>
+        <p> 19H15 - 20H45 </p> 
 
         </div>
 
