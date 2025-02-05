1f5f20bc918e218970f7b3bb441aa76d47380655
MergimHJ <mergimaskaj@hotmail.com>
2025-02-05 10:56:50 +0100

    accueil avec bouton et hero

---
+++ app/views/pages/accueil.php
@@ -0,0 +1,38 @@
+<section class="home-container">
+    <h1>HAGAKURE KARATE</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nisi veritatis cum voluptatibus doloremque commodi quam ab voluptates corrupti rerum. Molestiae, officiis ut reiciendis quasi officia nobis veniam eius ab?</p>
+    <a href="#contact"><button>Contactez-nous</button></a>
+    <a href="#horaire"><button>Horaires</button></a>
+    
+    <section id="#horaires">
+    <h2>Horaires</h2>
+    <ul>
+        <li>Mardi
+            <ul><li>
+                19h30/21h00
+            </li></ul>
+        </li>
+    </ul>
+    <ul>
+        <li>Mercredi
+            <ul><li>
+                16h00/17h30
+            </li></ul>
+        </li>
+    </ul>
+    <ul>
+        <li>Cours enfants
+            <ul>Vendredi</ul>
+            <ul><li>
+                18h00/19h15
+            </li></ul>
+        </li>
+        <li>Cours adultes
+            <ul><li>
+                19h15/20h45
+            </li></ul>
+        </li>
+    </ul>
+    <img src="/public\assets\img\horaires_img.png" alt="Image d'un dojo">
+    </section>
+</section>
--- app/views/pages/home.php
+++
@@ -1,4 +0,0 @@
-<section class="home-container">
-    <h1>Bienvenue chez Hagakure Karaté Club</h1>
-    <p>Découvrez notre passion pour le karaté et l'histoire de notre club.</p>
-</section>
