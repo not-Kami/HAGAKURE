00a0b78cc1902f783f630414da3df910b3931872
R3g3n7 <polidoroalessio9@gmail.com.com>
2025-02-06 10:33:00 +0100

    about section and general improvements

---
+++ app/views/components/about.php
@@ -0,0 +1,30 @@
+<section class="about">
+
+<div class="about-us">
+
+<div class="about-header">
+
+
+
+<h1> A PROPOS </h1>
+
+<p>  Plus qu’un simple sport, le karaté est un véritable outil d’inclusion 
+    et de développement personnel, où chaque pratiquant évolue à son propre
+     rythme dans un esprit de respect et de bienveillance. 
+     À travers nos enseignements, nous favorisons l’égalité des chances et encourageons 
+     chacun à repousser ses limites, à gagner en confiance et à s’épanouir tant sur le tatami 
+     que dans la vie quotidienne. </p>
+     <div class="button-container-2"> 
+
+<a href="#contact"><button class="btn-primary">Contactez-nous</button></a>
+<a href="#horaire"><button class="btn-secondary">Horaire</button></a>
+
+</div>
+
+
+</div>
+
+
+</div>
+
+</section>
