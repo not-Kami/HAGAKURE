fd004e2fb3442283a19b11ca754ceb4fb917493c
R3g3n7 <polidoroalessio9@gmail.com.com>
2025-02-06 01:09:01 +0100

    horaires / cards / kimono / license /

--- app/views/components/horaire.php
+++ app/views/components/horaire.php
@@ -1,16 +1,107 @@
+<div class="title">
+
+<h2>Horaires des Cours et Inscription</h2>
+
+<h3>Profitez de 3 Séances d'Essai Gratuites au Dojo!</h3>
+
+</title>
+
 <section class="horaire" id="#horaire">
 
-<h2>Horaires</h2>
+
 
 <div class="cards" >
 
- <h4> COURSE ADULTES </h4>
+ <h4>COURSE ADULTES</h4>
  <p> MARDI</p> 
  <p> 19H30 - 21H00</p> 
  <p> VENDREDI </p>
- <p> 19H15 - 20H45 </p> 
+ <p> 19H15 - 20H45</p> 
 
 </div>
 
-<img src="/public\assets\img\horaires_img.png" alt="Image d'un dojo">
+<div class="cards" >
+
+ <h4> Course ENFANTS </h4>
+ <p> MERCREDI</p> 
+ <p> 16H00 - 17H30 </p> 
+ <p> VENDREDI </p>
+ <p> 18H00 - 19H15 </p> 
+
+</div>
+
+
+
 </section>
+
+
+
+<section class="licence" id="#licence">
+
+<div class="file-box">
+
+<h3> Complétez et envoyez votre demande de licence JKA </h3>
+
+<div class="button-container-vertical">
+            <a href="#contact"><button class="btn-secondary">Télécharger le PDF</button></a>  <p>(Licence unique JKA 2023.pdf)</p>
+            
+        </div>
+
+
+
+</div>
+
+<div class="file-box">
+
+<h3> Découvrez tout sur la JKA, ses valeurs, ses grades et la pratique du karaté </h3>
+
+<div class="button-container-vertical">
+            <a href="#contact"><button class="btn-secondary">Télécharger le PDF</button></a> <p>((JKA-Livre-Infos_2019.pdf))</p>
+            
+            
+        </div>
+
+
+
+</div>
+
+
+
+</div>
+
+
+<div class="title">
+
+<h2> karate-gi Premium pour Tous les Combattants</h2>
+
+
+</title>
+
+
+<section class="horaire" id="#horaire">
+
+
+
+
+<div class="kimono-1" >
+
+ 
+
+</div>
+
+<div class="kimono-2" >
+
+
+
+</div>
+
+
+
+
+</section>
+<div class="button-container-vertical">
+            <a href="#contact"><button class="btn-primary">Commander</button></a> 
+            
+            
+        </div>
+
