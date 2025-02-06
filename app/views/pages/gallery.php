81e9a3f01abe07ca7509a9fe9110bf3f55ffd6c3
Kami <rokugaki.sama@gmail.com>
2025-02-06 10:21:26 +0100

    minor correction on gallery.css & link fix

--- app/views/pages/gallery.php
+++ app/views/pages/gallery.php
@@ -1,35 +1,34 @@
-
 <div class="parent">
-        <div class="div1">
-            <section>
-                <div class="square">
-                    <img src="public\assets\img\gallery\mukashi05.png" alt="Mukashi 5">
-                    <p>Mukashi Mukashi... (il était une fois...)</p>
-                </div>
-            </section>
+    <div class="div1">
+        <section>
+        <div class="square">
+            <img src="/public/assets/img/gallery/mukashi05.png" alt="Mukashi 5">
+            <p>Mukashi Mukashi... (il était une fois...)</p>
         </div>
-        <div class="div2">
-            <section>
-                <div class="square">
-                    <img src="public\assets\img\gallery\JKA03.png" alt="JKA 3">
-                    <p>Championnat JKA francophone 2013</p>
-                </div>
-            </section>
+        </section>
+    </div>
+    <div class="div2">
+        <section>
+        <div class="square">
+            <img src="/public/assets/img/gallery/JKA03.png" alt="JKA 3">
+            <p>Championnat JKA francophone 2013</p>
         </div>
-        <div class="div3">
-            <section>
-                <div class="square">
-                    <img src="public\assets\img\gallery\champ_belg02.png" alt="Championnat Belgique 1">
-                    <p>Championnat de Belgique JKA 2013</p>
-                </div>
-            </section>
+        </section>
+    </div>
+    <div class="div3">
+        <section>
+        <div class="square">
+            <img src="/public/assets/img/gallery/champ_belg02.png" alt="Championnat Belgique 1">
+            <p>Championnat de Belgique JKA 2013</p>
         </div>
-        <div class="div4">
-            <section>
-                <div class="square">
-                    <img src="public\assets\img\gallery\hagakure_img1.png" alt="Hagakure Cup 1">
-                    <p>Hagakure JKA Cup 2018</p>
-                </div>
-            </section>
+        </section>
+    </div>
+    <div class="div4">
+        <section>
+        <div class="square">
+            <img src="/public/assets/img/gallery/hagakure_img1.png" alt="Hagakure Cup 1">
+            <p>Hagakure JKA Cup 2018</p>
         </div>
+        </section>
+    </div>
     </div>
